94a01377014a759fbad47a289635029b7538b703
Adrian Hangan <iovuadrian@gmail.com>
2019-03-05 13:13:28 +0200

    import/export updates

--- etools-loading-mixin.js
+++ etools-loading-mixin.js
@@ -1,98 +1,103 @@
-import 'etools-loading.js';
-import _ from './node_modules/lodash-es/lodash.js';
+import './etools-loading.js';
+import _ from 'lodash-es';
 import { dedupingMixin } from '@polymer/polymer/lib/utils/mixin.js';
 
+/**
+ * @polymer
+ * @mixinFunction
+ */
+const internalLoadingMixin = baseClass => class extends baseClass {
+  connectedCallback() {
+    super.connectedCallback();
+    this.addEventListener('global-loading', this.handleLoading);
+    this.addEventListener('clear-loading-messages', this.clearLoadingQueue);
 
-  /*
-  * @polymer
-  * @mixinFunction
-  */
-  const internalLoadingMixin = baseClass => class extends baseClass {
-    connectedCallback() {
-      super.connectedCallback();
-      this.addEventListener('global-loading', this.handleLoading);
-      this.addEventListener('clear-loading-messages', this.clearLoadingQueue);
+    // create loading element, used for global loading
+    this.globalLoadingElement = this.createLoading();
+    this.globalLoadingElement.messages = [];
+  }
 
-      // create loading element, used for global loading
-      this.globalLoadingElement = this.createLoading();
-      this.globalLoadingElement.messages = [];
+  /**
+   * This method will create an etools-loading absolute element
+   * (loading element is appended to the body and it will cover entire screen)
+   * @param loadingMessage
+   * @returns {Element}
+   */
+  createLoading(loadingMessage) {
+    let newLoadingElement = document.createElement('etools-loading');
+    if (typeof loadingMessage === 'string' && loadingMessage !== '') {
+      newLoadingElement.loadingText = loadingMessage;
     }
+    newLoadingElement.absolute = true;
+    document.querySelector('body').appendChild(newLoadingElement);
+    return newLoadingElement;
+  }
 
-    /**
-     * This method will create an etools-loading absolute element
-     * (loading element is appended to the body and it will cover entire screen)
-     * @param loadingMessage
-     * @returns {Element}
-     */
-    createLoading(loadingMessage) {
-      let newLoadingElement = document.createElement('etools-loading');
-      if (typeof loadingMessage === 'string' && loadingMessage !== '') {
-        newLoadingElement.loadingText = loadingMessage;
-      }
-      newLoadingElement.absolute = true;
-      document.querySelector('body').appendChild(newLoadingElement);
-      return newLoadingElement;
-    }
-
-    /**
-     * Use this method to remove a loading element in the detached state of the element where loading is used
-     * @param loadingElement
-     */
-    removeLoading(loadingElement) {
-      if (loadingElement) {
-        document.querySelector('body').removeChild(loadingElement);
-      }
-    }
-
-    addMessageToQue(messages, source) {
-      let _messages = _.slice(messages);
-      _messages.push(source);
-      return _messages;
-    }
-
-    removeMessageFromQue(messages, source) {
-      let _messages = _.slice(messages);
-      _.remove(_messages, {loadingSource: source.loadingSource});
-      return _messages;
-    }
-
-    /**
-     * Show loading when data is requested from server, or save is in progress...
-     */
-    handleLoading(event) {
-      event.stopImmediatePropagation();
-      if (!this.globalLoadingElement) {
-        return;
-      }
-
-      let loadingSource = event.detail.loadingSource
-          ? event.detail.loadingSource
-          : _.get(event, 'path.0.localName', 'na');
-
-      if (event.detail.active) {
-        let message = _.get(event, 'detail.message', 'Loading...');
-        this.globalLoadingElement.messages = this.addMessageToQue(this.globalLoadingElement.messages, {
-          loadingSource: loadingSource,
-          message: message
-        });
-        this.globalLoadingElement.loadingText = _.last(this.globalLoadingElement.messages).message;
-        this.globalLoadingElement.active = true;
-      } else {
-        this.globalLoadingElement.messages = this.removeMessageFromQue(this.globalLoadingElement.messages, {
-          loadingSource: loadingSource
-        });
-        if (_.isEmpty(this.globalLoadingElement.messages)) {
-          this.globalLoadingElement.active = false;
-        } else {
-          this.globalLoadingElement.loadingText = _.last(this.globalLoadingElement.messages).message;
-        }
-      }
-    }
-
-    clearLoadingQueue(event) {
-      event.stopImmediatePropagation();
-      this.globalLoadingElement.messages = [];
-      this.globalLoadingElement.active = false;
+  /**
+   * Use this method to remove a loading element in the detached state of the element where loading is used
+   * @param loadingElement
+   */
+  removeLoading(loadingElement) {
+    if (loadingElement) {
+      document.querySelector('body').removeChild(loadingElement);
     }
   }
-  export default LoadingMixin = dedupingMixin(internalLoadingMixin);
+
+  addMessageToQue(messages, source) {
+    let _messages = _.slice(messages);
+    _messages.push(source);
+    return _messages;
+  }
+
+  removeMessageFromQue(messages, source) {
+    let _messages = _.slice(messages);
+    _.remove(_messages, {loadingSource: source.loadingSource});
+    return _messages;
+  }
+
+  /**
+   * Show loading when data is requested from server, or save is in progress...
+   */
+  handleLoading(event) {
+    event.stopImmediatePropagation();
+    if (!this.globalLoadingElement) {
+      return;
+    }
+
+    let loadingSource = event.detail.loadingSource
+        ? event.detail.loadingSource
+        : _.get(event, 'path.0.localName', 'na');
+
+    if (event.detail.active) {
+      let message = _.get(event, 'detail.message', 'Loading...');
+      this.globalLoadingElement.messages = this.addMessageToQue(this.globalLoadingElement.messages, {
+        loadingSource: loadingSource,
+        message: message
+      });
+      this.globalLoadingElement.loadingText = _.last(this.globalLoadingElement.messages).message;
+      this.globalLoadingElement.active = true;
+    } else {
+      this.globalLoadingElement.messages = this.removeMessageFromQue(this.globalLoadingElement.messages, {
+        loadingSource: loadingSource
+      });
+      if (_.isEmpty(this.globalLoadingElement.messages)) {
+        this.globalLoadingElement.active = false;
+      } else {
+        this.globalLoadingElement.loadingText = _.last(this.globalLoadingElement.messages).message;
+      }
+    }
+  }
+
+  clearLoadingQueue(event) {
+    event.stopImmediatePropagation();
+    this.globalLoadingElement.messages = [];
+    this.globalLoadingElement.active = false;
+  }
+};
+
+/**
+ * @polymer
+ * @mixinFunction
+ */
+const LoadingMixin = dedupingMixin(internalLoadingMixin);
+export default LoadingMixin;
